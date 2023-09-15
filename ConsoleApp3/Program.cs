f0ae3a2887f3f137da3da86093045a0a05be0aa6
RufetNesibov611 <rufaten@code.edu.az>
2023-09-15 14:14:56 -0700

    added homework c#1

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,186 @@
+﻿//task01
+
+//int n = 8;
+
+//if (n % 2 == 0)
+//{
+//    Console.WriteLine("Cutdur");
+//}
+//else
+//{
+//    Console.WriteLine("Tekdir");
+//}
+
+
+//task02
+//int m = 5;  
+//for (int i = 1; i <= m; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+//task03
+
+//int m = 5;
+
+//int sum = 0;
+
+//for(int i = 1; i <= m; i++)
+//{
+//    sum += i;
+//}
+
+//Console.WriteLine(sum);
+
+
+//task04
+
+//int m = 6;
+
+//int count = 0;
+
+//for (int i = 0; i <= m; i++)
+//{
+//    if (i % 2 == 1) count++;
+
+//}
+
+//Console.WriteLine(count);
+
+
+//task05
+
+//int n = 3;
+//int m = 8;
+//int sum = 0;
+//for (int  i = n;  i <=m; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        sum += i;
+//    }
+
+
+//}
+
+//Console.WriteLine(sum);
+
+
+//task06
+
+//int m = 5;
+
+//int fact = 1;
+
+//for(int i = 1; i <= m; i++)
+//{
+//    fact *= i;
+//}
+
+
+//Console.WriteLine(fact);   
+
+
+////task07
+
+//int[] nums = { 2, 3, 4, 5, };
+
+//int multipl = 1;
+
+//for(int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] % 2 == 0)
+//    {
+//        multipl *= nums[i];
+//    }
+//}
+
+
+//Console.WriteLine(multipl);
+
+
+
+////task08
+
+//using System.ComponentModel.Design;
+
+//int n = 33;
+
+//if (n % 3 == 0 && n % 7 == 0) 
+//{
+//    Console.WriteLine("bolunur");
+//}
+
+//else
+//{
+//    Console.WriteLine("bolunmur");
+//}
+
+//task09
+
+
+//int m = 6;
+
+//int n = 2;
+
+//int sum = 0;
+
+//if (m%2==0 && n % 2 == 0)
+//{
+//    sum = 1;
+
+//}
+//Console.WriteLine(m+n);
+
+
+//task10
+
+//int n = 4;
+
+//int m = 7;
+
+//int sum = 0;
+
+//for (int i = n; i <= m; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        sum += i;
+//    }
+//}
+//Console.WriteLine(sum);
+
+//task11
+
+
+int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+
+int sum = 0;
+
+for (int i = 0; i < nums.Length; i++)
+{
+    if (nums[i] % 2 == 1)
+    {
+        sum += nums[i];
+    }
+}
+
+Console.WriteLine(sum);
+
+
+//task12
+
+//int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+
+//int count = 1;
+
+//for (int i = 0; i<nums.Length; i++)
+//{
+//    if (nums[i]%2 == 0)
+//    {
+//        count += 1;
+
+//    }
+//}
+//Console.WriteLine(count);
